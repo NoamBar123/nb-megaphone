855b4ffe049bcddccea916f9717aefb804b1a714
Noam bar <62957473+NoamBar123@users.noreply.github.com>
2022-06-02 21:44:14 +0300

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -2,6 +2,5 @@ local QBCore = exports['qb-core']:GetCoreObject()
 
 QBCore.Functions.CreateUseableItem("megaphone", function(source, item)
     local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
     TriggerClientEvent('noam:megaphone123', src, item)
 end)
